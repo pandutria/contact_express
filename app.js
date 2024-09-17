294bb14d0193423121d28dc86e9070659dce85ab
pandutria <panduadyatama029@gmail.com>
2024-09-17 13:10:50 +0700

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ sequelize.sync().then(() => {
     console.log('Database synced');
 }).catch(err => console.log('Error syncing database: ' + err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
