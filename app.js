70ef84bc43306c5c63b44be967ff99988f22617a
pandutria <panduadyatama029@gmail.com>
2024-09-17 13:29:12 +0700

    update

--- app.js
+++ app.js
@@ -21,3 +21,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello from Express.js on Vercel!');
+  });
